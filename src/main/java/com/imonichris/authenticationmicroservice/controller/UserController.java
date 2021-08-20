3e9452dbb383db51d0a47bf72c07cbdbf8257626
chrisimoni <imonireal@gmail.com>
2021-08-20 22:25:31 +0100

    created repos, services and util function

---
+++ src/main/java/com/imonichris/authenticationmicroservice/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.imonichris.authenticationmicroservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @author Chris Imoni (https://github.com/chrisimoni)
+* @version 1.0
+*/
+@RestController
+public class UserController {
+
+}
